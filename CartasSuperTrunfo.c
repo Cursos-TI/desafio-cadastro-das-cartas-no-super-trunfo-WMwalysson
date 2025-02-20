1e152f52c79f4f2fda87d31185a4215cac65eb30
WMwalysson <202502092041@alunos.estacio.br>
2025-02-20 21:46:27 +0000

    coloquei quase tudo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,5 +20,59 @@ int main() {
 
     printf("Desafio das cartas no super trufo\n");
 
+    char estado [50];
+    char carta1 [50];
+    char cidade [50];
+    int populacao;
+    float area;
+    float pib;
+    int turisticos;
+
+    printf ("Digite o estado: \n");
+    scanf ("%s", estado);
+
+    printf ("Digite a carta1: \n");
+    scanf ("%s", carta1);
+
+    printf ("Digite a cidade: \n");
+    scanf ("%s", cidade);
+
+    printf ("População: \n");
+    scanf ("%d", &populacao);
+
+    printf ("Digite a area: \n");
+    scanf ("%f", &area);
+
+    printf ("Digite o pib: \n");
+    scanf ("%f", &pib);
+
+    printf ("Digite o número de pontos turisticos: \n");
+    scanf ("%d", &turisticos);
+
+    printf("Nome do estado: %s\nCarta: %s\nNome da cidade: %s\n", estado, carta1, cidade);
+    printf ("Populacao: %d\n, Area: %f\n, Pib: %f\n, Pontos turisticos: %d\n", populacao, area, pib, turisticos);
+
+     
+
+
+    return 0;
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
